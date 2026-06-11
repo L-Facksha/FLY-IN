120f2203fa977a65d22ca2037147e0af61338b86
PiziGa <azddinbahadi@gmail.com>
2026-06-11 22:05:53 +0100

    implementing algo

---
+++ src/parser/algorithm.py
@@ -0,0 +1,49 @@
+from graph import Graph
+
+
+class Dijkstra:
+    def __init__(self, graph: Graph) -> None:
+        self.graph = graph
+
+    def run(self, start: str, end: str) -> list[str]:
+        dist: dict[str, int] = {}
+        prev: dict[str, str] = {}
+        visited: set[str] = set()
+
+        for zone in self.graph.neighbors:
+            dist[zone] = 999999
+        dist[start] = 0
+
+        while True:
+            current: str | None = None
+            for zone in dist:
+                if zone not in visited:
+                    if current is None or dist[zone] < dist[current]:
+                        current = zone
+
+            if current is None or current == end:
+                break
+
+            visited.add(current)
+
+            for neighbor in self.graph.get_neighbors(current):
+                if neighbor in visited:
+                    continue
+                new_cost = dist[current] + \
+                    self.graph.get_cost(current, neighbor)
+                if new_cost < dist[neighbor]:
+                    dist[neighbor] = new_cost
+                    prev[neighbor] = current
+
+        path: list[str] = []
+        current = end
+        while current in prev:
+            path.append(current)
+            current = prev[current]
+        path.append(start)
+        path.reverse()
+
+        if path[0] != start:
+            return []
+
+        return path
