e2da3a9fbb82a0ea0350731501cd77f54efe5d01
piziga <azddinbahadi@gmail.com>
2026-06-30 20:51:52 +0100

    emplement visual

--- src/parser/algorithm.py
+++ src/parser/algorithm.py
@@ -34,23 +34,23 @@ class Dijkstra:
 
         all_paths: list[list[str]] = []
 
-        def dfs(node: str, path: list[str]) -> None:
-            if node == end:
+        def dfs(zone: str, path: list[str]) -> None:
+            if zone == end:
                 all_paths.append(list(path))
                 return
-            for nb in self.graph.get_neighbors(node):
-                if self.graph.zone_type.get(nb, 'normal') == 'blocked':
+            for neighbor in self.graph.get_neighbors(zone):
+                if self.graph.zone_type.get(neighbor, 'normal') == 'blocked':
                     continue
-                edge_cost = self.graph.get_cost(node, nb)
-                if dist[nb] == dist[node] + edge_cost:
-                    path.append(nb)
-                    dfs(nb, path)
+                cost_nex_zone = self.graph.get_cost(zone, neighbor)
+                if dist[neighbor] == dist[zone] + cost_nex_zone:
+                    path.append(neighbor)
+                    dfs(neighbor, path)
                     path.pop()
 
         dfs(start, [start])
 
-        if not all_paths:
-            return [self.run(start, end)]
+        # if not all_paths:
+        #     return [self.run(start, end)]
 
         def score(p: list[str]) -> tuple[int, int]:
             pri = sum(1 for z in p
@@ -60,53 +60,53 @@ class Dijkstra:
         all_paths.sort(key=score)
         return all_paths
 
-    def run(self, start: str, end: str) -> list[str]:
-        dist: dict[str, int] = {}
-        prev: dict[str, str] = {}
-        visited: set[str] = set()
+    # def run(self, start: str, end: str) -> list[str]:
+    #     dist: dict[str, int] = {}
+    #     prev: dict[str, str] = {}
+    #     visited: set[str] = set()
 
-        for zone in self.graph.neighbors:
-            dist[zone] = 9999
+    #     for zone in self.graph.neighbors:
+    #         dist[zone] = 9999
 
-        dist[start] = 0
+    #     dist[start] = 0
 
-        while True:
-            current: str | None = None
+    #     while True:
+    #         current: str | None = None
 
-            for zone in self.graph.neighbors:
-                if zone not in visited and dist[zone] < 9999:
-                    if current is None:
-                        current = zone
-                    elif dist[zone] < dist[current]:
-                        current = zone
-                    elif dist[zone] == dist[current]:
-                        if self.graph.zone_type.get(zone) == 'priority' and\
-                            self.graph.zone_type.get(current) != \
-                                'priority':
-                            current = zone
+    #         for zone in self.graph.neighbors:
+    #             if zone not in visited and dist[zone] < 9999:
+    #                 if current is None:
+    #                     current = zone
+    #                 elif dist[zone] < dist[current]:
+    #                     current = zone
+    #                 elif dist[zone] == dist[current]:
+    #                     if self.graph.zone_type.get(zone) == 'priority' and\
+    #                         self.graph.zone_type.get(current) != \
+    #                             'priority':
+    #                         current = zone
 
-            if current is None or current == end:
-                break
+    #         if current is None or current == end:
+    #             break
 
-            visited.add(current)
-            for neighbor in self.graph.get_neighbors(current):
-                if neighbor in visited:
-                    continue
-                new_cost = dist[current] + \
-                    self.graph.get_cost(current, neighbor)
+    #         visited.add(current)
+    #         for neighbor in self.graph.get_neighbors(current):
+    #             if neighbor in visited:
+    #                 continue
+    #             new_cost = dist[current] + \
+    #                 self.graph.get_cost(current, neighbor)
 
-                if new_cost < dist[neighbor]:
-                    dist[neighbor] = new_cost
-                    prev[neighbor] = current
+    #             if new_cost < dist[neighbor]:
+    #                 dist[neighbor] = new_cost
+    #                 prev[neighbor] = current
 
-        if start != end and end not in prev:
-            return []
-        path: list[str] = []
-        current = end
-        while current in prev:
-            path.append(current)
-            current = prev[current]
+    #     if start != end and end not in prev:
+    #         return []
+    #     path: list[str] = []
+    #     current = end
+    #     while current in prev:
+    #         path.append(current)
+    #         current = prev[current]
 
-        path.append(start)
-        path.reverse()
-        return path
+    #     path.append(start)
+    #     path.reverse()
+    #     return path
